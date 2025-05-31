e09e84a062757ba48b730ab7cc6a41ef19b6c83b
Santiago Cuesta <scuesta988@gmail.com>
2025-05-30 21:34:37 -0500

    bubble debugging

--- Alvaro/bubblesort.py
+++ Alvaro/bubblesort.py
@@ -1,6 +1,10 @@
+debug = True
+
 def bubblesort(data, key):
     n = len(data)
     for i in range(n):
+        if debug:
+            print(f"Pass {i + 1}")
         for j in range(0, n - i - 1):
             if int(data[j][key]) > int(data[j + 1][key]):
                 data[j], data[j + 1] = data[j + 1], data[j]
